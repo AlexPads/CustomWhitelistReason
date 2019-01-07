e1111494edbba29bc86d9c13aa65b025d58a71c7
iZeaoGamer <jameshayward14@icloud.com>
2019-01-07 13:05:09 +0000

    Added ban-message and reasons (Untested)
    I added this so it can be more "custom" and more better.

--- src/Assassiner354/CustomWhitelistReason/Main.php
+++ src/Assassiner354/CustomWhitelistReason/Main.php
@@ -39,13 +39,28 @@ class Main extends PluginBase implements Listener {
     public function onPreLogin(PlayerPreLoginEvent $event) {
 		$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 		$reason = $cfg->get("whitelist.reason");
+	    	
 		$player = $event->getPlayer();
 		$name = $player->getName();
+	    
 		
 		if(!$player->isWhitelisted($name)) {
-			$msg = str_replace(["{reason}", "{line}", "&"], [$reason, "\n", "§"], $cfg->get("whitelist.message"));
-			$msg = str_replace(["{line}", "&"], ["\n", "§"], $cfg->get("whitelist.reason")); //To-do see if this method works.
-			$player->close("", $msg);
+			$whitelistMessage = str_replace(["{reason}", "{line}", "&"], [$reason, "\n", "§"], $cfg->get("whitelist.message"));
+			$whitelistMessage = str_replace(["{line}", "&"], ["\n", "§"], $cfg->get("whitelist.reason")); //To-do see if this method works.
+			$player->close("", $whitelistedMessage);
 		}
+	    //Custom banned system:
+	         if(!$player->isBanned($name)) {
+	    $banList = $player->getServer()->getNameBans();
+	    $banEntry = $banList->getEntries();
+            $entry = $banEntry[strtolower($name)];
+                $reason = $entry->getReason();
+                if ($reason != null || $reason != "") {
+                       $bannedMessage = str_replace(["{line}", "&", "{reason}"], ["\n", "§", $reason], $cfg->get("banned-message")); 
+		} else {
+			$bannedMessage = str_replace(["{line}", "&"], ["\n", "§"], $cfg->get("no-banned-reason-message"));
+			$player->close("", $bannedMessage);
+                }
 	}
 }
+	}
